158108195e196654b095ededb91841813770f023
luksen1991 <lukasz1991code@gmail.com>
2018-02-27 18:03:19 +0100

    Create simple server

--- src/main/java/pl/javaserver/HelloServer/HelloServerApplication.java
+++ src/main/java/pl/javaserver/HelloServer/HelloServerApplication.java
@@ -1,12 +1,25 @@
 package pl.javaserver.HelloServer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.ipc.netty.http.server.HttpServer;
 
-@SpringBootApplication
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+
 public class HelloServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloServerApplication.class, args);
+
+		RouterFunction route = route( GET("/"),
+				request -> ServerResponse.ok().body(fromObject("Hello World Server")));
+		HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
+		HttpServer server = HttpServer.create("localhost",8080);
+		server.startAndAwait(new ReactorHttpHandlerAdapter(httpHandler));
 	}
 }

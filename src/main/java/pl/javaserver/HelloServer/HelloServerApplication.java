65eb803d15a11b0c6b50b89878dcd4487a98ce96
luksen1991 <lukasz1991code@gmail.com>
2018-02-27 15:33:46 +0100

    Create project Spring IO

---
+++ src/main/java/pl/javaserver/HelloServer/HelloServerApplication.java
@@ -0,0 +1,12 @@
+package pl.javaserver.HelloServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+}

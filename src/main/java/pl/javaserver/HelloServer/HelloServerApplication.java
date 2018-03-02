3b0d42cba37c5bbb43c6cf5bf2906db42a26e7c2
luksen1991 <lukasz1991code@gmail.com>
2018-03-02 01:05:28 +0100

    Send time to front

--- src/main/java/pl/javaserver/HelloServer/HelloServerApplication.java
+++ src/main/java/pl/javaserver/HelloServer/HelloServerApplication.java
@@ -1,11 +1,19 @@
 package pl.javaserver.HelloServer;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.ipc.netty.http.server.HttpServer;
+
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
@@ -14,12 +22,28 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 
 public class HelloServerApplication {
 
-	public static void main(String[] args) {
 
-		RouterFunction route = route( GET("/"),
-				request -> ServerResponse.ok().body(fromObject("Hello World Server")));
-		HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
-		HttpServer server = HttpServer.create("localhost",8080);
-		server.startAndAwait(new ReactorHttpHandlerAdapter(httpHandler));
-	}
+
+    public static void main(String[] args) {
+        new HelloServerApplication().serve();
+    }
+
+    private void serve(){
+        RouterFunction route = route(GET("/api/time"),
+                getTime());
+
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
+        HttpServer server = HttpServer.create("localhost", 8080);
+        server.startAndAwait(new ReactorHttpHandlerAdapter(httpHandler));
+    }
+    private HandlerFunction<ServerResponse> getTime() {
+        return request -> {
+            LocalDateTime currentTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+            return ServerResponse.ok()
+                    .contentType(new MediaType(MediaType.TEXT_PLAIN, Charset.forName("utf-8")))
+                    .body(fromObject(formatter.format(currentTime)));
+        };
+    }
 }
